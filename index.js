c5e2879ec16f9d4ff9be671a4681890a58780dd3
Amber Mohler <ambermohler@imac.lan>
2019-11-12 09:21:58 -0500

    still working on second problem

--- index.js
+++ index.js
@@ -61,8 +61,13 @@ function makePersonObject(id, name, email) {
  * passing { id: 1, name: 'Leia', email: 'leia@leia.com` } as the argument,
  * the returned value should look like `Hello, my name is Leia`.
 */
-function getName(/* code here */) {
-  /* code here */
+function getName(id, name, email) {
+  let person = {
+    id: id,
+    name: name,
+    email: email
+  };
+  return 'Hello, my name is ' + ${name};
 }
 
 /**
